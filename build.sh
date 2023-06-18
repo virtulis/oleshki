cf8d9aa2444cf6149e3ff76898f164f3154681a4
Danko Aleksejevs <danko@very.lv>
2023-06-18 11:39:25 +0300

    Better build tools

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-pnpm exec tsc &&
-pnpm exec esbuild --bundle src/web/app.tsx --inject:src/web/inject.ts --outdir=dist --sourcemap &&
-pnpm exec esbuild --bundle src/web/worker.ts --outdir=dist --sourcemap &&
-pnpm exec sass src/app.scss dist/app.css && cp node_modules/core-js-bundle/minified.js dist/core.js &&
-pnpm exec babel --presets @babel/preset-env dist/app.js  -o dist/legacy.js --source-maps
