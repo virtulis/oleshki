e6160eebf070e708f3c3816c5d2c0811f212e0f4
Danko Aleksejevs <danko@very.lv>
2023-06-21 14:45:23 +0300

    Save google token

--- src/oauth.ts
+++ src/oauth.ts
@@ -1,10 +1,12 @@
 import { OAuth2Client } from 'google-auth-library';
 import { config } from './common.js';
+import { writeFile } from 'fs/promises';
 
 const auth = new OAuth2Client(config.googleApiKey);
 console.log(auth.generateAuthUrl({ scope: 'https://www.googleapis.com/auth/spreadsheets' }));
 
 const { tokens } = await auth.getToken(process.argv[2]);
 console.log(tokens);
-
-auth.setCredentials(tokens);
+if (tokens) {
+	await writeFile('config.json', JSON.stringify({ ...config, googleOAuthToken: tokens }, null, '\t'));
+}
