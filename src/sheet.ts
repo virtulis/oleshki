8beceb359ecaf477561e409cd38e6ebc018c95a6
Danko Aleksejevs <danko@very.lv>
2023-07-01 01:58:32 +0300

    Name parsing

--- src/sheet.ts
+++ src/sheet.ts
@@ -14,6 +14,7 @@ import path from 'path';
 import { renderToString } from 'react-dom/server';
 import { createElement } from 'react';
 import { DiffTable } from './web/diff.js';
+import damlev from 'damerau-levenshtein';
 
 const execFile = promisify(child_process.execFile);
 
@@ -242,41 +243,141 @@ async function dumpNames() {
 		name1: Maybe<string>;
 		name2: Maybe<string>;
 		name3: Maybe<string>;
+		name: Maybe<string>;
+		surname: Maybe<string>;
+		patronym: Maybe<string>;
 		year: Maybe<string>;
+		status: Maybe<string>;
+		text: Maybe<string>;
 	};
 	const output: Row[] = [];
+	
+	const norm = (s: string) => s[0].toUpperCase() + s.slice(1).toLowerCase();
+	const normArr = (a: Maybe<string>[]) => a.filter(isSome).map(norm);
+	
+	const suffixes: Record<string, number> = {};
+	const allNames = new Set<string>(
+		(Object.values(JSON.parse(await readFile('src/data/names.json', 'utf8'))).flat() as string[]).filter(isSome).filter(s => s.length).map(norm)
+	);
+	const allNamesList = [...allNames];
+	const allNameRegexes = allNamesList.map(name => new RegExp(`(^|[\\s\\n])(${name})`, 'ui'));
 	
 	for (const entry of list.entries) {
 		
 		console.log('----', entry.id);
 		
+		const batch: Row[] = [];
+		
 		for (const field of ['contact', 'contactInfo', 'details'] as const) {
 			
 			const src = entry[field];
-			if (!src?.trim()) continue;
+			if (!src || !src?.trim()) continue;
 			console.log(src);
 			
-			const matches = src.matchAll(
-				/((?<!\p{Alpha})\p{Lu}\p{Ll}+) +(\p{Lu}\.|\p{Lu}\p{Ll}{3,}) *(\p{Lu}\.|\p{Lu}\p{Ll}{3,})?[ ,)(]+(\d{1,2}[,.]\d{1,2}[,.]\d{4}|19\d{2}|2[012]\d{2}|\d{1,3} *[лрг+])?/gu
-			);
+			const matches = [
+				/(?<name1>(?<!\p{Alpha})\p{Lu}[\p{Ll}'"`’]+) +(?<name2>\p{Lu}\.|\p{Lu}[\p{Ll}'"`’]{3,}) *(?<name3>\p{Lu}\.|\p{Lu}[\p{Ll}'"`’]{3,})?/gu,
+				/(?<name1>\p{Lu}[\p{Alpha}'"`’]+) +(?<name2>\p{Lu}[\p{Alpha}'"`’]+) +(?<name3>\p{Lu}[\p{Alpha}'"`’]+(ьича?|вича?|вн[ау]|ЬИЧА?|ВИЧА?|ВН[АУ]))/gu,
+				/(?<name1>\p{Lu}[\p{Alpha}'"`’]+(нко|ова|кий|ина|ков|чук|ька|кая|іна|нов|ева|ник|лов|нюк|ець|єва|вич|хін|сюк|НКО|ОВА|КИЙ|ИНА|КОВ|ЧУК|ЬКА|КАЯ|ІНА|НОВ|ЕВА|НИК|ЛОВ|НЮК|ЕЦЬ|ЄВА|ВИЧ|ХІН|СЮК)) +(?<name2>\p{Lu}([\p{Alpha}'"`’]+|\.?)) *(?<name3>\p{Lu}([\p{Alpha}'"`’]+|\.?))?/gu,
+				// /(?<name2>[\p{Alpha}'"`’]+)+ (?<name1>[\p{Alpha}'"`’]+(нко|ова|кий|ина|ков|чук|ька|кая|іна|нов|ева|ник|лов|нюк|ець|єва|хін|сюк))/gui,
+				// /(?<name1>[\p{Alpha}'"`’]+) (?<name2>[\p{Alpha}'"`’]+) (?<name3>[\p{Alpha}'"`’]+)/gui
+			].flatMap(reg => [...src.matchAll(reg)]);
 			// console.log([...matches]);
 			
 			for (const match of matches) {
-				const [full, name1, name2, name3, year] = [...match];
+				const { name1, name2, name3 } = match.groups!;
+				const year = src.slice(match.index! + match[0].length).match(/^[ ,)(]*(\d{1,2}[,.]\d{1,2}[,.]\d{4}|19\d{2}|2[012]\d{2}|\d{1,3} *[лрг+])?/)?.[1];
 				const row: Row = {
 					entry: entry.id,
 					field,
 					name1,
 					name2,
 					name3,
+					name: null,
+					surname: null,
+					patronym: null,
 					year,
+					status: entry.status,
+					text: src?.replace(/\s+/mg, ' '),
 				};
-				output.push(row);
+				batch.push(row);
 				console.log(JSON.stringify(row));
+				// if (name1) {
+				// 	suffixes[name1.slice(-3)] = (suffixes[name1.slice(-3)] || 0) + 1;
+				// }
 			}
 			
 		}
 		
+		if (!batch.length) {
+			for (const field of ['contact', 'contactInfo', 'details'] as const) {
+				const src = entry[field];
+				if (!src) continue;
+				for (const regex of allNameRegexes) {
+					const match = src.match(regex);
+					if (!match) continue;
+					const name = match[1];
+					batch.push(<Row> {
+						entry: entry.id,
+						field,
+						name1: null,
+						name2: name,
+						name3: null,
+						name,
+						surname: null,
+						patronym: null,
+						year: null,
+						status: entry.status,
+						text: src?.replace(/\s+/mg, ' '),
+					});
+				}
+			}
+		}
+		
+		batch.sort((a, b) => a.name1?.localeCompare(b.name1 ?? a.name1) || a.name2?.localeCompare(b.name2 ?? a.name2) || a.name3?.localeCompare(b.name3 ?? a.name3) || 0);
+		const deduped = batch.filter((row, i) => !(row.name1 == batch[i - 1]?.name1 && row.name2 == batch[i - 1]?.name2 && row.name3 == batch[i - 1]?.name3));
+		output.push(...deduped);
+		
+	}
+	
+	// console.log(Object.entries(suffixes).sort((a, b) => b[1] - a[1]).map(s => s[0]).slice(0, 20).join('|'));
+	
+	
+	// console.log(allNames);
+	
+	const maybeNotNames = new Set<string>();
+	
+	for (const row of output) {
+	
+		const { name1, name2, name3 } = row;
+		
+		row.name = (
+			normArr([name2, name1, name3]).find(c => allNames.has(c))
+			?? normArr([name2, name1, name3]).filter(n => n.length > 3).find(c => allNamesList.find(o => damlev(o, c).steps < 2))
+		);
+		
+		row.patronym = normArr([name3, name2]).filter(n => n != row.name).find(c => c.match(/(ьича?|вича?|вн[ау]|їна)$/ui));
+		
+		if (!row.name) normArr([name1, name2, name3]).filter(s => s.length > 2).forEach(n => maybeNotNames.add(n));
+		row.name ??= (name3 ? [name2, name1, name3] : [name1, name2, name3]).filter(isSome).map(norm).find(n => n != row.patronym);
+		
+		row.surname = normArr([name1, name2, name3]).find(n => n != row.name && n != row.patronym);
+		
+		row.patronym ??= normArr([name3])[0];
+		
+	}
+	
+	await writeFile('data/maybe-not-names.json', JSON.stringify([...maybeNotNames].sort(), null, '\t'));
+	
+	await writeFile('data/names.hmm.csv', stringify(output.filter(
+		r => [r.name1, r.name2, r.name3].filter(isSome).length != [r.name, r.surname, r.patronym].filter(isSome).length
+	), { header: true }));
+	
+	for (const k of ['name', 'surname', 'patronym'] as const) {
+		const uniq = new Set<string>();
+		for (const row of output) {
+			if (row[k]) uniq.add(row[k]!);
+		}
+		await writeFile(`data/uniq.${k}.txt`, [...uniq].join('\n'));
 	}
 	
 	await writeFile('data/names.json', JSON.stringify(output, null, '\t'));
